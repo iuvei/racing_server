00bab505daf2db57f82aa23c189648bb24049c2d
maomao <maomao.li@jjcargo.com>
2016-10-10 22:08:30 +0800

    robot rate

--- racing_server/src/main/java/com/racing/controller/user/UserController.java
+++ racing_server/src/main/java/com/racing/controller/user/UserController.java
@@ -1,7 +1,9 @@
 package com.racing.controller.user;
 
 import com.racing.controller.vo.UserVo;
+import com.racing.model.po.WechatClient;
 import com.racing.service.user.UserService;
+import com.racing.service.user.WechatClientService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     UserService userService;
+    @Autowired
+    WechatClientService wechatClientService;
 
     /**
      * 分盘个人信息管理修改个人信息
@@ -36,5 +40,26 @@ public class UserController {
     public Object selectNicName(@PathVariable Integer userId,
                                 @RequestParam String nicName) {
         return userService.selectNicName(userId, nicName);
+    }
+
+    @ApiOperation("分盘-机器人管理-是否报盘")
+    @RequestMapping(value = "/{userId}/robot/enable", method = RequestMethod.PUT)
+    public Object robotIsEnable(@PathVariable Integer userId,
+                                @RequestBody UserVo userVo) {
+        return userService.updateRobotIsEnable(userId, userVo.getClientIsEnable());
+    }
+
+    @ApiOperation("分盘-机器人管理-机器人赔率列表")
+    @RequestMapping(value = "/{userId}/robot/rate", method = RequestMethod.GET)
+    public Object robotRate(@PathVariable Integer userId,
+                            @RequestParam(required = false, defaultValue = "1") Integer page) {
+        return wechatClientService.select(userId, page);
+    }
+
+    @ApiOperation("分盘-机器人管理-修改机器人赔率")
+    @RequestMapping(value = "/{userId}/robot/rate", method = RequestMethod.PUT)
+    public Object robotRate(@PathVariable Integer userId,
+                            @RequestBody WechatClient wechatClient) {
+        return wechatClientService.update(userId, wechatClient);
     }
 }
