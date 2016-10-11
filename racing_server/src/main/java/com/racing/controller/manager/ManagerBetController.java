88b08dd107db8633c81fabfabf24cd6ceedd569e
maomao <maomao.li@jjcargo.com>
2016-10-11 23:52:54 +0800

    fix bug

--- racing_server/src/main/java/com/racing/controller/manager/ManagerBetController.java
+++ racing_server/src/main/java/com/racing/controller/manager/ManagerBetController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 
 @RestController
-@RequestMapping("/manger")
+@RequestMapping("/manager")
 public class ManagerBetController {
 
     @Autowired
