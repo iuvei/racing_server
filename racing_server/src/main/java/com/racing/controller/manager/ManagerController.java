bae06dba7312fb0dceb2a170a82df9e11d0583ad
maomao <maomao.li@jjcargo.com>
2016-10-11 20:57:20 +0800

    add user api

--- racing_server/src/main/java/com/racing/controller/manager/ManagerController.java
+++ racing_server/src/main/java/com/racing/controller/manager/ManagerController.java
@@ -1,6 +1,7 @@
 package com.racing.controller.manager;
 
 import com.racing.controller.vo.manager.ModifyManagerInfoVo;
+import com.racing.model.po.Manager;
 import com.racing.service.manager.ManagerService;
 import com.racing.util.LoginStatusSaveUtil;
 import io.swagger.annotations.ApiOperation;
@@ -10,50 +11,55 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ManagerController {
 
-  @Autowired
-  private ManagerService managerService;
+    @Autowired
+    private ManagerService managerService;
 
 
-  /**
-   * 获取全部总盘管理员信息
-   * 
-   * @return
-   */
-  @ApiOperation(value="总盘-用户管理-总盘用户管理-获取全部用户")
-  @RequestMapping(value = "/manager", method = RequestMethod.GET)
-  public Object getManagerList() {
-    return managerService.getManagerInfoVo();
-  }
+    /**
+     * 获取全部总盘管理员信息
+     *
+     * @return
+     */
+    @ApiOperation(value = "总盘-用户管理-总盘用户管理-获取全部用户")
+    @RequestMapping(value = "/manager", method = RequestMethod.GET)
+    public Object getManagerList() {
+        return managerService.getManagerInfoVo();
+    }
 
-  // @RequestMapping(value = "/manager", method = RequestMethod.POST)
-  // public Object addNewManager() {
-  // return managerService.getManagerInfoVo();
-  // }
+    // @RequestMapping(value = "/manager", method = RequestMethod.POST)
+    // public Object addNewManager() {
+    // return managerService.getManagerInfoVo();
+    // }
 
-  @ApiOperation(value="总盘-用户管理-总盘用户管理-禁用用户")
-  @RequestMapping(value = "/manager/{managerId}/status/disable", method = RequestMethod.PUT)
-  public Object updateManagerDisable(@PathVariable("managerId") int managerId) {
-    return managerService.updateMangerIsEnable(managerId, false);
-  }
+    @ApiOperation(value = "总盘-用户管理-总盘用户管理-禁用用户")
+    @RequestMapping(value = "/manager/{managerId}/status/disable", method = RequestMethod.PUT)
+    public Object updateManagerDisable(@PathVariable("managerId") int managerId) {
+        return managerService.updateMangerIsEnable(managerId, false);
+    }
 
-  @ApiOperation(value="总盘-用户管理-总盘用户管理-启用用户")
-  @RequestMapping(value = "/manager/{managerId}/status/enable", method = RequestMethod.PUT)
-  public Object updateManagerEnable(@PathVariable("managerId") int managerId) {
-    return managerService.updateMangerIsEnable(managerId, true);
-  }
+    @ApiOperation(value = "总盘-用户管理-总盘用户管理-启用用户")
+    @RequestMapping(value = "/manager/{managerId}/status/enable", method = RequestMethod.PUT)
+    public Object updateManagerEnable(@PathVariable("managerId") int managerId) {
+        return managerService.updateMangerIsEnable(managerId, true);
+    }
 
-  @ApiOperation(value="总盘-用户管理-总盘用户管理-修改用户")
-  @RequestMapping(value = "/manager/{managerId}", method = RequestMethod.PUT)
-  public Object updateManagerInfo(@PathVariable("managerId") int managerId, @RequestBody ModifyManagerInfoVo vo) {
-    Integer nowLoginManagerId = LoginStatusSaveUtil.getManagerId();
-    return managerService.updateMangerInfo(nowLoginManagerId, managerId, vo);
-  }
+    @ApiOperation(value = "总盘-用户管理-总盘用户管理-修改用户")
+    @RequestMapping(value = "/manager/{managerId}", method = RequestMethod.PUT)
+    public Object updateManagerInfo(@PathVariable("managerId") int managerId, @RequestBody ModifyManagerInfoVo vo) {
+        Integer nowLoginManagerId = LoginStatusSaveUtil.getManagerId();
+        return managerService.updateMangerInfo(nowLoginManagerId, managerId, vo);
+    }
 
-  @ApiOperation(value="总盘-用户管理-总盘用户管理-删除用户")
-  @RequestMapping(value = "/manager/{managerId}", method = RequestMethod.DELETE)
-  public Object deleteManagerInfo(@PathVariable("managerId") int managerId) {
-    Integer nowLoginManagerId = LoginStatusSaveUtil.getManagerId();
-    return managerService.deleteMangerInfo(nowLoginManagerId, managerId);
-  }
+    @ApiOperation(value = "总盘-用户管理-总盘用户管理-删除用户")
+    @RequestMapping(value = "/manager/{managerId}", method = RequestMethod.DELETE)
+    public Object deleteManagerInfo(@PathVariable("managerId") int managerId) {
+        Integer nowLoginManagerId = LoginStatusSaveUtil.getManagerId();
+        return managerService.deleteMangerInfo(nowLoginManagerId, managerId);
+    }
 
+    @ApiOperation(value = "总盘-用户管理-总盘用户管理-新增用户")
+    @RequestMapping(value = "/manager", method = RequestMethod.POST)
+    public Object addManagerInfo(@RequestBody Manager manager) {
+        return managerService.addManger(manager.getNickName(), manager.getUserName(), manager.getPassword());
+    }
 }
