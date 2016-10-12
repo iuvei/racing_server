33fec010242326d886a312ce3c77bbc2960fd204
binqi.mao <binqi.mao@dianrong.com>
2016-10-12 23:54:51 +0800

    modify

--- racing_server/src/main/java/com/racing/model/repo/MembersRepo.java
+++ racing_server/src/main/java/com/racing/model/repo/MembersRepo.java
@@ -7,7 +7,6 @@ import com.racing.util.PageUtil;
 import jodd.util.StringUtil;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
