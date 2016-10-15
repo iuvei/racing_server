6326a88d0bb6ee3ed65b6cc26733648595f00439
maomao <maomao.li@jjcargo.com>
2016-10-15 16:51:08 +0800

    user result

--- racing_server/src/main/java/com/racing/service/manager/UserRacingIncomeService.java
+++ racing_server/src/main/java/com/racing/service/manager/UserRacingIncomeService.java
@@ -1,10 +1,13 @@
 package com.racing.service.manager;
 
 import com.racing.controller.vo.ApiResult;
+import com.racing.controller.vo.RecordResultVo;
+import com.racing.model.po.RecordResult;
 import com.racing.model.po.UserRacingIncome;
 import com.racing.model.repo.RecordResultRepo;
 import com.racing.model.repo.UserRacingIncomeRepo;
 import com.racing.model.repo.UserRepo;
+import com.racing.model.stake.util.RecordResultPOUtil;
 import com.racing.util.PageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,6 +43,23 @@ public class UserRacingIncomeService {
             return ApiResult.createErrorReuslt("分盘不存在");
         }
         List<UserRacingIncome> list = userRacingIncomeRepo.selectByRacingNum(userId, racingNum);
-        return ApiResult.createSuccessReuslt(list.get(0));
+
+        if (list != null && list.get(0).getIsComplateStatistics()) {
+            RecordResult recordResult = recordResultRepo.getRecordResultByRacingNum(racingNum);
+            RecordResultVo resultVo = new RecordResultVo();
+            resultVo.setRacingNum(recordResult.getRacingNum());
+            resultVo.setResult(RecordResultPOUtil.convertResult(recordResult));
+            resultVo.setFirstAddSecond(recordResult.getFirstAddSecond());
+            resultVo.setIsFifthUp(recordResult.getIsFifthUp());
+            resultVo.setIsFirstSecondBig(recordResult.getIsFirstSecondBig());
+            resultVo.setIsFirstSecondOdd(recordResult.getIsFirstSecondOdd());
+            resultVo.setIsFirstUp(recordResult.getIsFirstUp());
+            resultVo.setIsFourthUp(recordResult.getIsFourthUp());
+            resultVo.setIsSecondUp(recordResult.getIsSecondUp());
+            resultVo.setIsThirdUp(recordResult.getIsThirdUp());
+            return ApiResult.createSuccessReuslt(resultVo);
+        }
+
+        return ApiResult.createErrorReuslt("没开奖");
     }
 }
