bc0e4a76863d1537261b8705d37c1a03106ca073
binqi.mao <binqi.mao@dianrong.com>
2016-10-16 02:57:47 +0800

    MODIFY

--- racing_server/src/main/java/com/racing/service/timetask/ScheduledRandomService.java
+++ racing_server/src/main/java/com/racing/service/timetask/ScheduledRandomService.java
@@ -48,12 +48,12 @@ public class ScheduledRandomService {
    * 生成的信息是9点7分开始第一场比赛，之后每5分钟一场，即第二场比赛9点12分
    */
 //  @Scheduled(cron = "0 11 12 * * ?")
-  @Scheduled(cron = "0 50 8 * * ?")
+//  @Scheduled(cron = "0 50 8 * * ?")
   public void reportCurrentTime() {
 
-    Date racingStartTime = DateUtil.setDateHHmmss(new Date(), 9, 7, 0);
+    Date racingStartTime = DateUtil.setDateHHmmss(new Date(), 2, 7, 0);
 
-    for (int i = 0; i < 200; i++) {
+    for (int i = 0; i < 300; i++) {
       // for (int i = 0; i < 179; i++) {
       int target = new Random().nextInt(CalculationHandle.allResult.size());
 
@@ -106,9 +106,9 @@ public class ScheduledRandomService {
   
   /**
    * 计算最优解
-   * 每天9点~23点的6分7秒开始，每5分钟一次（9点7分开始第一场比赛，6分7秒开始计算，即当前比赛倒计时53秒时）
+   * 每天9点~23点的1分7秒开始，每5分钟一次（9点7分开始第一场比赛，6分7秒开始计算，即当前比赛倒计时53秒时）
    */
-  @Scheduled(cron = "7 6/5 9-23 * * ?")
+  @Scheduled(cron = "7 1/5 0-23 * * ?")
   public void invokeDealRecordResultOptimalCalculation() {
 	  totalStatisticsService.dealRecordResultOptimalCalculation();
   }
@@ -117,7 +117,7 @@ public class ScheduledRandomService {
    * 统计总盘的按期号盈亏和押注（取当前时间的上一场比赛，进行统计）
    * 每天9点~23点7分5秒开始，没五分钟一次（9点7分开始第一场比赛，7分5秒开始统计上一场比赛结果，即新一轮比赛倒计时295秒时）
    */
-  @Scheduled(cron = "5 7/5 9-23 * * ?")
+  @Scheduled(cron = "5 2/5 0-23 * * ?")
   public void invokeDealTotalIncome(){
 	  totalStatisticsService.dealTotalIncome();
   }
@@ -132,7 +132,7 @@ public class ScheduledRandomService {
    * 统计分盘的按期号盈亏和押注（取当前时间的上一场比赛，进行统计）
    * 每天9点~23点7分10秒开始，每五分钟一次（9点7分开始第一场比赛，7分10秒开始统计上一场比赛结果，即新一轮比赛倒计时290秒时）
    */
-  @Scheduled(cron = "10 7/5 9-23 * * ?")
+  @Scheduled(cron = "10 2/5 0-23 * * ?")
   public void invokeDealUserIncome(){
 	  List<User> userList = userRepo.selectUser();
 	  for(User user : userList){
