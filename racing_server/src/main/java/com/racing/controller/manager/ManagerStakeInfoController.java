7e5b314fdb321f956cc15cc2b0bef968f6583066
binqi.mao <binqi.mao@dianrong.com>
2016-10-16 18:15:50 +0800

    modify

--- racing_server/src/main/java/com/racing/controller/manager/ManagerStakeInfoController.java
+++ racing_server/src/main/java/com/racing/controller/manager/ManagerStakeInfoController.java
@@ -1,8 +1,12 @@
 package com.racing.controller.manager;
 
+import com.racing.controller.vo.ApiResult;
 import com.racing.service.stake.StakeInfoService;
+import com.racing.util.LoginStatusSaveUtil;
+
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,4 +32,15 @@ public class ManagerStakeInfoController {
 		return stakeInfoService.getManagerStakeInfoByDay(day);
 	}
 	
+	@ApiOperation("总盘--分盘押注报表—-押注情况-按期号查")
+	@RequestMapping(value = "/manager/user/{userId}/stake/racingnum/info", method = RequestMethod.GET)
+	public Object getUserStakeInfoByRacingNum(@RequestParam("racingNum")String racingNum, @PathVariable("userId")Integer userId){
+		return stakeInfoService.getUserStakeInfoByRacingNum(userId,racingNum);
+	}
+
+	@ApiOperation("总盘--分盘押注报表—-押注情况-按日期查")
+	@RequestMapping(value = "/manager/user/{userId/stake/day/info", method = RequestMethod.GET)
+	public Object getUserStakeInfoByDay(@RequestParam("day")String day, @PathVariable("userId")Integer userId) throws ParseException{
+		return stakeInfoService.getUserStakeInfoByDay(userId, day);
+	}
 }
