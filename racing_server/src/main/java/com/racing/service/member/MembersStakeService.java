bfd9d1aaac71270ce99eb2e2035486f74733f94d
binqi.mao <binqi.mao@dianrong.com>
2016-10-20 02:14:00 +0800

    modify

--- racing_server/src/main/java/com/racing/service/member/MembersStakeService.java
+++ racing_server/src/main/java/com/racing/service/member/MembersStakeService.java
@@ -6,6 +6,10 @@ import com.racing.model.po.MemberStake;
 import com.racing.model.po.Members;
 import com.racing.model.repo.MembersRepo;
 import com.racing.model.repo.MembersStakeRepo;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,7 @@ import java.util.List;
 
 @Service
 public class MembersStakeService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MembersStakeService.class);
 
     @Autowired
     MembersStakeRepo membersStakeRepo;
@@ -22,21 +27,28 @@ public class MembersStakeService {
 
 
     public Object selectByWechatSn(Integer userId, List<String> wechatSns, String racingNum) {
+    	LOGGER.info("userId is : {}", userId);
+    	LOGGER.info("racingNum is : {}", racingNum);
         List<MembersStakeClientVO> voList=new ArrayList<>();
         List<Members> membersList=new ArrayList<>();
         wechatSns.stream()
             .forEach(a -> {
+            	LOGGER.info("wechatSns is : {}", a);
                 Members members = membersRepo.getByWechatSnAndUserId(a, userId);
                 membersList.add(members);
             });
-        membersList.stream()
-            .forEach(a->{
-                MemberStake memberStake=membersStakeRepo.selectByMembersIdAndTacingNum(a.getId(),racingNum);
-                MembersStakeClientVO vo=new MembersStakeClientVO();
-                vo.setMembers(a);
-                vo.setMemberStake(memberStake);
-                voList.add(vo);
-            });
+        if(CollectionUtils.isNotEmpty(membersList)){
+        	membersList.stream()
+        	.forEach(a->{
+        		MemberStake memberStake=membersStakeRepo.selectByMembersIdAndTacingNum(a.getId(),racingNum);
+        		if(memberStake!=null){
+        			MembersStakeClientVO vo=new MembersStakeClientVO();
+        			vo.setMembers(a);
+        			vo.setMemberStake(memberStake);
+        			voList.add(vo);
+        		}
+        	});
+        }
         return ApiResult.createSuccessReuslt(voList);
     }
 
