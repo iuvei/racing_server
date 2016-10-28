3ec6fb80eb65e5cefc19051ad5f8a894d898cac4
binqi.mao <binqi.mao@dianrong.com>
2016-10-28 21:39:24 +0800

    modify

--- racing_server/src/main/java/com/racing/config/WebMvcConfig.java
+++ racing_server/src/main/java/com/racing/config/WebMvcConfig.java
@@ -1,25 +1,20 @@
 package com.racing.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig extends WebMvcConfigurerAdapter{
 
 
-  @Bean
-  public CorsFilter corsFilter() {
-    final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-    final CorsConfiguration corsConfiguration = new CorsConfiguration();
-    corsConfiguration.setAllowCredentials(true);
-    corsConfiguration.addAllowedOrigin("*");
-    corsConfiguration.addAllowedHeader("*");
-    corsConfiguration.addAllowedMethod("*");
-    urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-    return new CorsFilter(urlBasedCorsConfigurationSource);
-  }
+	@Override  
+    public void addCorsMappings(CorsRegistry registry) {  
+        registry.addMapping("/**")  
+                .allowedOrigins("*")  
+                .allowCredentials(true)  
+                .allowedMethods("GET", "POST", "DELETE", "PUT","OPTIONS")  
+                .maxAge(3600);  
+    }  
 
 }
